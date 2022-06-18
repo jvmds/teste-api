45daa66bd32578a6a729c154f828c1777b87030d
joao.santos-at-298894534260 <joaovitormelodossantos.1@gmail.com>
2022-06-18 18:07:34 -0300

    teste2

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'TesteApi.urls'
+ROOT_URLCONF = 'DjangoApp.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'TesteApi.wsgi.application'
+WSGI_APPLICATION = 'DjangoApp.wsgi.application'
 
 
 # Database
